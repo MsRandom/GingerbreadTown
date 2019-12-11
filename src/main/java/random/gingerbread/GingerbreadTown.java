e221667b36640c020d95b035cb10eaa94abad4f8
MsRandom1 <ahmedtrg981106@gmail.com>
2019-12-11 11:45:56 +0300

    @Mod cause we need a mod

---
+++ src/main/java/random/gingerbread/GingerbreadTown.java
@@ -0,0 +1,9 @@
+package random.gingerbread;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GingerbreadTown.MOD_ID)
+public class GingerbreadTown {
+
+    public static final String MOD_ID = "gingerbread";
+}
